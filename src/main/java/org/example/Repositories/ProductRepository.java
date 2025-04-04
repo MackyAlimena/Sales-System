cf2eb1e6b73d0e93fc1e794bdc823d0635b412bb
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2025-04-04 10:11:07 -0300

    Package for repositories Implementation for ProductRepository with methods to handle operations for product

---
+++ src/main/java/org/example/Repositories/ProductRepository.java
@@ -0,0 +1,47 @@
+package org.example.Repositories;
+
+import org.example.model.Product;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class ProductRepository {
+    // This class will handle the database operations related to products
+    // Porducts is a Map with the product id as the key and the product as the value, because the id is unique
+    private Map<Integer, Product> products;
+
+    public ProductRepository() {
+        this.products = new HashMap<>();
+    }
+
+    public void addProduct(Product product) {
+        products.put(product.getProductId(), product);
+    }
+
+    public void updateProduct(Product product) {
+        int id = product.getProductId();
+        if (products.containsKey(id)) {
+            products.put(id, product);
+        }
+        throw new IllegalArgumentException("Product with id " + id + " does not exist.");
+    }
+
+    public void deleteProduct(int productId) {
+        products.remove(productId);
+    }
+
+    public Product getProductById(int productId) {
+        return products.get(productId);
+    }
+
+    public List<Product> getAll() {
+        return new ArrayList<>(products.values());
+    }
+
+    public boolean existsById(int productId) {
+        return products.containsKey(productId);
+    }
+}
