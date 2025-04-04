1e7d23b2b08158812b7b232ad0c2aa6ba760c50a
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2025-04-04 09:33:31 -0300

    Salesman class implementation with attributes, getters and setters

---
+++ src/main/java/org/example/model/Salesman.java
@@ -0,0 +1,44 @@
+package org.example.model;
+
+public class Salesman {
+    //Salesman that works at the store
+    private int salesman_id;//primary key, unique
+    private String name;
+    private int salary;
+
+    public Salesman(int salesman_id, String name, int salary) {
+        this.salesman_id = salesman_id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    //Get salesman id
+    public int getSalesman_id() {
+        return salesman_id;
+    }
+
+    //Set salesman id
+    public void setSalesman_id(int salesman_id) {
+        this.salesman_id = salesman_id;
+    }
+
+    //Get salesman name
+    public String getName() {
+        return name;
+    }
+
+    //Set salesman name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //Get salesman salary
+    public int getSalary() {
+        return salary;
+    }
+
+    //Set salesman salary
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
